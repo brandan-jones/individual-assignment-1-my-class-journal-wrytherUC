86e7dd7feaca43f0a1830f92ff81902bb8c34e8c
wryther <rytherwy@mail.uc.edu>
2023-10-02 22:27:49 -0400

    Added Springboot annotation Service

--- src/main/java/edu/uc/cech/soit/myclassjournal/service/JournalService.java
+++ src/main/java/edu/uc/cech/soit/myclassjournal/service/JournalService.java
@@ -1,9 +1,11 @@
 package edu.uc.cech.soit.myclassjournal.service;
 
 import edu.uc.cech.soit.myclassjournal.dto.JournalEntry;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class JournalService implements IJournalService{
     @Override
     public void save(JournalEntry journalEntry) {
