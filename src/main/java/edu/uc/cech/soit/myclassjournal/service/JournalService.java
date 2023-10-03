9768b023f89164fe300591032c6e291933a303b5
wryther <rytherwy@mail.uc.edu>
2023-10-02 22:25:51 -0400

    Created JournalService, implements IJournalService

---
+++ src/main/java/edu/uc/cech/soit/myclassjournal/service/JournalService.java
@@ -0,0 +1,17 @@
+package edu.uc.cech.soit.myclassjournal.service;
+
+import edu.uc.cech.soit.myclassjournal.dto.JournalEntry;
+
+import java.util.List;
+
+public class JournalService implements IJournalService{
+    @Override
+    public void save(JournalEntry journalEntry) {
+
+    }
+
+    @Override
+    public List<JournalEntry> fetchAll() {
+        return null;
+    }
+}
