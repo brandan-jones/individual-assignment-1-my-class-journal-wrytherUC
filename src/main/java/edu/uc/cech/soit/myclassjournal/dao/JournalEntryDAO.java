f86fb3c51bd4db5c7f3bfbf5e2e3eb8f7a7ad6c2
wryther <rytherwy@mail.uc.edu>
2023-10-02 22:55:12 -0400

    Added map, updated fetchAll method to use map

--- src/main/java/edu/uc/cech/soit/myclassjournal/dao/JournalEntryDAO.java
+++ src/main/java/edu/uc/cech/soit/myclassjournal/dao/JournalEntryDAO.java
@@ -3,9 +3,25 @@ package edu.uc.cech.soit.myclassjournal.dao;
 import edu.uc.cech.soit.myclassjournal.dto.JournalEntry;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Repository
 public class JournalEntryDAO implements IJournalEntryDAO{
+
+    Map<Integer, JournalEntry> allJournalEntries = new HashMap<>();
+
     public JournalEntry save(JournalEntry journalEntry) {
+        Integer journalEntryId = journalEntry.getJournalEntryId();
+        allJournalEntries.put(journalEntryId, journalEntry);
         return journalEntry;
+    }
+
+    @Override
+    public List<JournalEntry> fetchAll() {
+        List<JournalEntry> returnJournalEntries = new ArrayList<>(allJournalEntries.values());
+        return returnJournalEntries;
     }
 }
